4b95d146685ef70517d18d39e7b77d8bd1ca7561
Harsh Vardhan Chawla <148744120+HarsHvardhnn@users.noreply.github.com>
2024-04-14 21:39:30 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 // mongodb+srv://harshvchawla996:ZeKEWIzV0BgDxxJz@cluster0.ehvlcx1.mongodb.net/manthan
 const dbConnect  = async () =>{
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
+    mongoose.connect('mongodb+srv://harshvchawla996:ZeKEWIzV0BgDxxJz@cluster0.ehvlcx1.mongodb.net/manthan').then(()=>{
         console.log('database connected');
 
     }).catch((err) => {
