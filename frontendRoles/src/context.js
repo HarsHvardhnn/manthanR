48d943460eab19d30ff4a78e662e1e1dc1beb0ce
Harsh Vardhan Chawla <148744120+HarsHvardhnn@users.noreply.github.com>
2024-04-15 15:51:34 +0530

    Delete frontendRoles directory

--- frontendRoles/src/context.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-
-export const userContext = createContext("");
-export const authContext = createContext("");
-export const adminContext = createContext("");
-export const superadminContext = createContext("");
