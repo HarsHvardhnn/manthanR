032560a56633c8d78df1a730fd07c26f95ae5457
harshvardhanataladinn <harshvardhanchawla@aladinnserver.in>
2024-04-20 23:33:59 +0530

    logout button

--- frontendRoles/src/components/Home/Header.jsx
+++ frontendRoles/src/components/Home/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Logo from "./Header.png";
@@ -6,6 +6,12 @@ import Logo from "./Header.png";
 function Header() {
   const navigate = useNavigate();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [token,setToken]= useState(null);
+  useEffect(()=>{
+    const token= localStorage.getItem('token');
+    setToken(token);
+
+  },[])
 
   const handleMouseEnter = () => {
     setIsDropdownOpen(true);
@@ -36,14 +42,26 @@ function Header() {
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         >
-          <button
-            className=" px-2 py-1 sm:px-4 sm:py-2 rounded-full text-xs font-extrabold sm:text-base sm:mb-2 md:mb-0 text-blue-600 border-2 border-blue-600 hover:bg-blue-600 hover:text-white"
-            aria-haspopup="true"
-            aria-expanded={isDropdownOpen ? "true" : "false"}
-          >
-            Admin
-          </button>
-          {isDropdownOpen && (
+      {
+        token ? (    <button
+          className=" px-2 py-1 sm:px-4 sm:py-2 rounded-full text-xs font-extrabold sm:text-base sm:mb-2 md:mb-0 text-blue-600 border-2 border-blue-600 hover:bg-blue-600 hover:text-white"
+          aria-haspopup="true"
+          aria-expanded={isDropdownOpen ? "true" : "false"}
+          onClick={()=>{
+            localStorage.removeItem('token');
+            navigate('/login')
+          }}
+        >
+          logout
+        </button>):(    <button
+          className=" px-2 py-1 sm:px-4 sm:py-2 rounded-full text-xs font-extrabold sm:text-base sm:mb-2 md:mb-0 text-blue-600 border-2 border-blue-600 hover:bg-blue-600 hover:text-white"
+          aria-haspopup="true"
+          aria-expanded={isDropdownOpen ? "true" : "false"}
+        >
+          Admin
+        </button>)
+      }
+          { !token && isDropdownOpen && (
             <div className="absolute right-0 sm:left-0 z-10 w-fit bg-white rounded-md shadow-lg ">
               <button
                 onClick={() => {
