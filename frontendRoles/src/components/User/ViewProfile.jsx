0d2f12afff23f962be51116e6205b9ea651fd064
avinashsingh108 <avinash.singh.24646@gmail.com>
2024-04-29 20:37:17 +0530

    changes

--- frontendRoles/src/components/User/ViewProfile.jsx
+++ frontendRoles/src/components/User/ViewProfile.jsx
@@ -72,10 +72,6 @@ const ViewProfile = ({ onClose ,loading ,getUser,data }) => {
           {data?.dept}
         </p>
         <p>
-          <span className="font-semibold">Course:</span>{" "}
-          {data?.dept}
-        </p>
-        <p>
           <span className="font-semibold">Semester:</span>{" "}
           {data?.semester}
         </p>
