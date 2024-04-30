3befae22682c2c0972767c3440a00d98e3e2585b
avinashsingh108 <avinash.singh.24646@gmail.com>
2024-05-01 00:28:55 +0530

    changes

--- frontendRoles/src/components/User/Chatbot.jsx
+++ frontendRoles/src/components/User/Chatbot.jsx
@@ -250,7 +250,7 @@ const Chatbot = () => {
             </div>
             <div className="hidden md:flex">
               <p className="py-2 px-6 bg-white rounded-xl font-bold text-base ml-2">
-                Hello {user.username} ✨
+                Hello Sophia ✨
               </p>
             </div>
           </div>
@@ -422,7 +422,7 @@ const Chatbot = () => {
                           <p className="sm:p-1 text-xl md:text-2xl lg:text-3xl bg-gray-200 border rounded-md">
                             😞
                           </p>
-                          <p className=" max-w-10 sm:max-w-14 text-[8px] sm:text-[10px] leading-snug sm:leading-normal ">Strongly Diagree</p>
+                          <p className=" max-w-10 sm:max-w-14 text-[8px] sm:text-[10px] leading-snug sm:leading-normal ">Strongly Disagree</p>
                         </div>
                       </button>
                     </div>
