063e9de95a3249aeaea3c5825b4fc589f6f7c76f
Avinash Singh <137880543+avinashsingh108@users.noreply.github.com>
2024-09-11 17:14:51 +0530

    Update otpService.js

--- otpService.js
+++ otpService.js
@@ -9,8 +9,8 @@ async function sendOTP(email) {
 
   let transporter = nodemailer.createTransport({
     host: "smtp-mail.outlook.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: "manowealth@iitp.ac.in",
       pass: "CRwnf613#@!",
