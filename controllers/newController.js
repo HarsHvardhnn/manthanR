d30bceca370c82432e1562a63b7618fedd1c0910
Harsh <harshvchawla997@gmail.com>
2024-10-05 15:15:27 +0530

    user insert flow revamped

--- controllers/newController.js
+++ controllers/newController.js
@@ -3,46 +3,42 @@ const userModel = require('../models/userSchema');
 const jwt = require('jsonwebtoken')
 const insertQuestions = async (req, res) => {
     try {
-        const token = req.headers.authorization; 
+      const { email } = req.decoded;
+      const { user_response, answers, score } = req.body;
   
-        const { email, user_response, answers, score } = req.body;
-
-        const userPromise = userModel.findOne({ email }, { _id: 1 });
-        const updateScorePromise = userModel.findOneAndUpdate(
-            { email },
-            { $set: { score, score_date: new Date() } },
-            { new: true }
-        );
-
-        const [user, updatedUser] = await Promise.all([userPromise, updateScorePromise]);
-
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        
-        const mappedQuestions = answers.map(qa => ({
-            question: qa.question,
-            answer: qa.answer
-        }));
-
-        const newQuestionsDoc = new FiftyQuestionsModel({
-            user: user._id,
-            questions: mappedQuestions,
-            user_response
-        });
-
-        await newQuestionsDoc.save();
-
-        return res.status(201).json({ message: "Questions inserted successfully" });
+      const updatedUser = await userModel.findOneAndUpdate(
+        { email },
+        { $set: { score, score_date: new Date() } },
+        { new: true, projection: { _id: 1 } }
+      );
+  
+      if (!updatedUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      const mappedQuestions = answers.map(qa => ({
+        question: qa.question,
+        answer: qa.answer
+      }));
+  
+      const newQuestionsDoc = new FiftyQuestionsModel({
+        user: updatedUser._id,
+        questions: mappedQuestions,
+        user_response
+      });
+  
+      await newQuestionsDoc.save();
+  
+      return res.status(201).json({ message: "Questions inserted successfully" });
     } catch (error) {
-        console.error("Error:", error);
-        if (error.name === 'JsonWebTokenError') {
-            return res.status(401).json({ message: "Unauthorized" });
-        }
-        return res.status(500).json({ message: "Internal server error" });
+      console.error("Error:", error);
+      if (error.name === 'JsonWebTokenError') {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      return res.status(500).json({ message: "Internal server error" });
     }
-};
+  };
+  
 
 
 const getUsers = async (req, res) => {
